efab0ded5f7a30edb5605640bf433947bb47c03a
Vladimir Ortiz Dev <vladimir.ortiz.dev@gmail.com>
2025-11-02 10:14:10 -0600

    Se agregaron los menuItems

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,34 @@
+
+import 'package:flutter/material.dart';
+
+class MenuItem{
+  final String titile;
+  final String subTutle;
+  final String link;
+  final IconData icon;
+
+  MenuItem({
+    required this.titile,
+    required this.subTutle,
+    required this.link,
+    required this.icon
+  });
+
+
+
+}
+
+List<MenuItem> appMenuItem = [
+  MenuItem(
+    titile: "Botones",
+    subTutle: "Varios botones en flutter",
+    link: "/buttons",
+    icon: Icons.smart_button_outlined
+  ),
+  MenuItem(
+    titile: "Tarjetas",
+    subTutle: "Un contenedor estilizado",
+    link: "/card",
+    icon: Icons.credit_card
+  )
+]; 
