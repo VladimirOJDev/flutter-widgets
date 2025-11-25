08e4ae3d697bfa7b80a0560776a3410935ee5b90
Vladimir Ortiz Dev <vladimir.ortiz.dev@gmail.com>
2025-11-24 22:23:43 -0600

    Screens creadas

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,7 +1,13 @@
-export 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
-export 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
-export 'package:widgets_app/presentation/screens/home/home_screen.dart';
-
 //Archivo encargado de exportar las screens, 
 //sirve para que al momento de importar en otro archivo solo tengamos
 //una sola importacion para cada screen y to individualmente
+
+export 'package:widgets_app/presentation/screens/animated/animated_screen.dart';
+export 'package:widgets_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
+export 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
+export 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
+export 'package:widgets_app/presentation/screens/home/home_screen.dart';
+export 'package:widgets_app/presentation/screens/infinite_scroll/infinite_scrool_screen.dart';
+export 'package:widgets_app/presentation/screens/progress/progress_screen.dart';
+export 'package:widgets_app/presentation/screens/snack_bar/snack_bar.dart';
+export 'package:widgets_app/presentation/screens/ui_controls/ui_controls_screen.dart';
